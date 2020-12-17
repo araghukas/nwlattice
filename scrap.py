260dd961b599e0dd1457d0eb748ae8474e9be7e1
araghukas <ghukasa@mcmaster.ca>
2020-12-17 11:58:53 -0500

    setup ignores tests package

--- scrap.py
+++
@@ -1,6 +0,0 @@
-import random
-
-
-def get_next(N):
-    for i in range(N):
-        yield random.randint(0, 30)
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from nwlattice import __version__
 
 setup(
@@ -7,6 +7,6 @@ setup(
     description="A package for generating atom positions in nanowire "
                 "structures",
     author="Ara Ghukasyan",
-    install_requires=['numpy'],
-    packages=find_packages()
+    install_requires=["numpy"],
+    packages=["nwlattice"]
 )
