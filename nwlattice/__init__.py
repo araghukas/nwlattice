8cf520f374199f0d858a38fa20fd45dde9bbf586
araghukas <ghukasa@mcmaster.ca>
2020-12-29 20:43:46 -0500

    implement get_info function

--- nwlattice/__init__.py
+++ nwlattice/__init__.py
@@ -6,6 +6,7 @@ def get_info(type_name: str = None) -> None:
     printed.
     """
     from inspect import getmembers
+    from difflib import get_close_matches
     import nwlattice.base as base
     import nwlattice.nw as nw
     obj_list = getmembers(nw)
@@ -22,22 +23,30 @@ def get_info(type_name: str = None) -> None:
 
     type_names = []
     for nw_class in nw_class_list:
-        type_names.append(str(nw_class).lstrip("<class ").rstrip(">"))
+        name = str(nw_class).lstrip("<class '").rstrip("'>")
+        name = name.split('.')[-1]
+        type_names.append(name)
 
     if type_name:
         type_name_index = None
         exists = False
         for i, tn in enumerate(type_names):
-            if type_name in tn:
+            if type_name == tn:
                 exists = True
                 type_name_index = i
                 break
+
         if not exists:
             print("nanowire type `%s` not found in nwlattice.nw" % type_name)
+            matches = get_close_matches(type_name, type_names)
+            if matches:
+                print("did you mean: `%s`?" % matches[0])
             return
+
         print(type_name)
-        print(nw_class_list[type_name_index].__doc__)
-        print(nw_class_list[type_name_index].__init__.__doc__)
+        print(nw_class_list[type_name_index].__doc__[1:])
+        print(nw_class_list[type_name_index].__init__.__doc__[1:])
+
     else:
         for type_name, nw_class in zip(type_names, nw_class_list):
             print(type_name)
